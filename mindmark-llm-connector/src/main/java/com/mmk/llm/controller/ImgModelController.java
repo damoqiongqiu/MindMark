fcb3fc174e64d75cde52af195c5d7e0673b111f9
kenan2016 <xwy524871763qq@gmail.com>
2024-12-20 17:04:47 +0800

    智谱文生图

---
+++ mindmark-llm-connector/src/main/java/com/mmk/llm/controller/ImgModelController.java
@@ -0,0 +1,35 @@
+package com.mmk.llm.controller;
+
+import com.mmk.llm.service.ImgModelService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.image.Image;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kenan
+ * 图片模型控制器
+ * 用来处理智谱 AI 的图像生成模型
+ */
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("llm")
+public class ImgModelController {
+
+    private ImgModelService imgModelService;
+
+    /**
+     * 根据文本生成图片
+     *
+     * @param msg
+     * @return image: 图片的 base64 编码或者图片的 url
+     */
+    @GetMapping("/genImg")
+    public Image genImg(@RequestParam(value = "msg", defaultValue = "根据大漠孤烟直，长河落日圆这句古诗生成一张图片") String msg) {
+        return this.imgModelService.genImg(msg);
+    }
+}
---
+++ mindmark-llm-connector/src/main/java/com/mmk/llm/service/zhipu/ZhiPuImgModelService.java
@@ -0,0 +1,36 @@
+package com.mmk.llm.service.zhipu;
+
+import com.mmk.llm.config.AiModelEnum;
+import com.mmk.llm.service.ImgModelService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.image.Image;
+import org.springframework.ai.image.ImageModel;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.zhipuai.ZhiPuAiImageOptions;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author kenan
+ * 智谱文生图服务
+ * 详细请参考 https://bigmodel.cn/dev/api/image-model/cogview
+ */
+@Slf4j
+@Service
+@AllArgsConstructor
+public class ZhiPuImgModelService implements ImgModelService {
+
+    private final ImageModel imageModel;
+
+    @Override
+    public Image genImg(String msg) {
+        // 如果需要自定义参数，可以参考 ZhiPuAiImageOptions，同时务必参考智谱官方文档（因 AI 开放平台迭代迅速，各家厂商配置项变化快）
+        ImageResponse response = imageModel.call(
+                new ImagePrompt(msg,
+                        ZhiPuAiImageOptions.builder().withModel(AiModelEnum.ZHI_PU_MODEL_COG_VIEW3_PLUS.getCode())
+                                .build())
+        );
+        return response.getResult().getOutput();
+    }
+}
