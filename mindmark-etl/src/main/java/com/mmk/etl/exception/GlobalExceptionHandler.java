0b80a8adae91d8e880a144b1a5dbc8646c7f74c5
ZhibinJiang <ZhibinJiang@chatlabs.cn>
2025-03-08 17:37:18 +0800

    feat(i18n/messages.properties): 添加文件上传大小限制提示
    在 messages.properties 和 messages_en.properties 中添加了文件上传大小限制的国际化提示信息。
    
    feat(GlobalExceptionHandler.java): 实现文件上传大小限制异常处理的国际化
    
    在 GlobalExceptionHandler.java 中实现了文件上传大小超过限制时的异常处理，并通过 I18nUtil 获取国际化提示信息。

--- mindmark-etl/src/main/java/com/mmk/etl/exception/GlobalExceptionHandler.java
+++ mindmark-etl/src/main/java/com/mmk/etl/exception/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.mmk.etl.exception;
 
+import com.mmk.rbac.i18n.I18nUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
-import org.springframework.http.ResponseEntity;
+
+import java.util.Locale;
 
 /**
  *  TODO: 统一异常在这里写，暂时先实现 上传文件大小超过限制时的异常处理
@@ -12,10 +16,16 @@ import org.springframework.http.ResponseEntity;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maxFileSize;
+
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     @ResponseBody
-    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException e) {
-        // TODO 提醒文本国际化
-        return ResponseEntity.badRequest().body("The file size must not be larger than 5MB.");
+    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException e, Locale locale) {
+        String message = I18nUtil.getMessage(
+                "upload.size.exceeded",
+                new Object[]{maxFileSize}
+        );
+        return ResponseEntity.badRequest().body(message);
     }
 }
